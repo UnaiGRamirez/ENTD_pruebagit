16adef9de2a201a5b76efd2c42995944d8a31fca
Iker Palenzuela <ikdgr@plaiaundi.net>
2024-12-11 10:36:01 +0100

    commit iker

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, rama Nueva!");
         System.out.println("Hola Unai soy Iker");
-       
+       System.out.println();
     }
 }
