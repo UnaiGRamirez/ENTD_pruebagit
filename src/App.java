db7c0b848dcc77dc2d20a623991cbc9fbd6f85ce
Iker Palenzuela <ikdgr@plaiaundi.net>
2024-12-11 10:31:59 +0100

    commit mensaje a unai

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, rama Nueva!");
+        System.out.println("Hola Unai soy Iker");
        
     }
 }
